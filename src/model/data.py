f84cf35d898320bb087415e0fd5bfa3c80a980f0
emdemor <emdemor415@gmail.com>
2022-06-04 23:53:25 -0300

    imp: add simpler example

--- src/model/data.py
+++ src/model/data.py
@@ -9,37 +9,10 @@ from src.model import __version__
 
 def make_dataset(config, download_bases=False):
 
-    targets = [
-        "pastry",
-        "z_scratch",
-        "k_scatch",
-        "stains",
-        "dirtiness",
-        "bumps",
-        "other_faults",
-    ]
+    data = pd.read_csv(config["data_local_path"])
 
-    if download_bases:
+    X = data.drop(columns="Drug", errors="ignore")
 
-        download_file(url=config["data_url"], destination=config["data_local_path"])
-
-        download_file(
-            url=config["columns_url"], destination=config["columns_local_path"]
-        )
-
-    columns = list(
-        map(
-            to_snake_case,
-            read_file_string(config["columns_local_path"], splitlines=True),
-        )
-    )
-
-    data = pd.read_csv(config["data_local_path"], sep="\t", header=None)
-
-    data.columns = columns
-
-    X = data.drop(columns=targets, errors="ignore")
-
-    y = np.sum(data[targets] * np.arange(1, 1 + len(targets)).reshape(1, -1), axis=1)
+    y = data["Drug"]
 
     return X, y
--- src/model/features.py
+++ src/model/features.py
@@ -2,3 +2,34 @@ import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
+
+def build_features(X: pd.DataFrame, y: pd.Series) -> tuple:
+
+    X["Sex"] = X["Sex"].replace(
+        {
+            "F": 0,
+            "M": 1,
+        }
+    )
+
+    X["BP_LOW"] = (X["BP"] == "LOW").astype("Int8")
+    X["BP_HIGH"] = (X["BP"] == "HIGH").astype("Int8")
+
+    X["Cholesterol"] = X["Cholesterol"].replace(
+        {
+            "NORMAL": 0,
+            "HIGH": 1,
+        }
+    )
+
+    y = y.replace(
+        {
+            "drugA": 1,
+            "drugB": 2,
+            "drugC": 3,
+            "drugX": 4,
+            "DrugY": 5,
+        }
+    )
+
+    return X, y
