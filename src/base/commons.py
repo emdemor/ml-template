503725e9517a27b9fb9d9ddf529c14692951f2e4
emdemor <emdemor415@gmail.com>
2022-06-07 23:27:14 -0300

    IMP refactor of to_snake_case function

--- src/base/commons.py
+++ src/base/commons.py
@@ -3,24 +3,56 @@ import git
 import pandas as pd
 
 
-def get_last_git_tag():
+def get_last_git_tag() -> str:
+    """
+    Get the latest git tag.
+
+    Returns
+    -------
+    str
+        Latest git tag
+    """
 
     repo = git.Repo()
 
-    latest_tag = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)[-1].name
+    latest_tag = None
+
+    try:
+        latest_tag = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)[
+            -1
+        ].name
+
+    except IndexError:
+        raise IndexError(
+            "No git tags found. You can add one through `git tag <tag_name>`"
+        )
 
     return latest_tag
 
 
-def to_snake_case(name):
+def to_snake_case(string: str) -> str:
+    """Converts a string to snake case.
 
-    name = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
+    Parameters
+    ----------
+    string : str
+        Any input string
 
-    name = re.sub("([a-z0-9])([A-Z])", r"\1_\2", name).lower()
+    Returns
+    -------
+    str
+        The string converted to snake case format
+    """
+    string = string.strip().replace(" ", "_")
 
-    name = name.replace("__", "_")
+    string = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", string)
 
-    return name
+    string = re.sub("([a-z0-9])([A-Z])", r"\1_\2", string).lower()
+
+    while "__" in string:
+        string = string.replace("__", "_")
+
+    return string
 
 
 def dataframe_transformer(dataframe, transformer):
